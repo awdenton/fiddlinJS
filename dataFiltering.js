40417b9c87d4399b35e4eec0af934451c9fe0b09
awdenton <awdenton@gmail.com>
2021-08-07 18:01:19 -0400

    initial commit

---
+++ dataFiltering.js
@@ -0,0 +1,27 @@
+let matryoshka = {
+    name: "Katie",
+    eyeColor: "Blue",
+    hairColor: "Black",
+    content: {
+        name: "Danielle",
+        eyeColor: "Hazel",
+        hobbies: ["Visiting petting zoos", "Whaling", "QAnon"],
+        hairColor: "Blonde",
+        content: {
+            name: "Esmerelda",
+            hairColor: "Brown",
+            eyeColor: "Brown"
+        }
+    }
+};
+
+function getNames(obj) {
+    if(!obj.hasOwnProperty('content')) {
+        console.log(obj.name);
+    } else {
+        console.log(obj.name);
+        getNames(obj.content);
+    }
+}
+
+getNames(matryoshka);
